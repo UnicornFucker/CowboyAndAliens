c96e08352634936a762d48516a64869ecf67dc9e
Thorben Brühl <thorben.bruehl@data-room.de>
2014-03-28 11:27:09 +0100

    Grundgerüst erstellt

---
+++ gwt/src/de/comyoutech/cowboyandalien/client/GwtLauncher.java
@@ -0,0 +1,19 @@
+package de.comyoutech.cowboyandalien.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import de.comyoutech.cowboyandalien.MyGdxGame;
+
+public class GwtLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new MyGdxGame();
+        }
+}
