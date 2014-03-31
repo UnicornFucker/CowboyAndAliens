03d60003b6e154c0f4c0283701696856d0ec8996
flexus385 <lappen@lappen.de>
2014-03-31 21:22:51 +0200

    Test

---
+++ core/src/de/comyoutech/cowboyandalien/FelixKannJetzAuchKlassenErstellen.java
@@ -0,0 +1,13 @@
+package de.comyoutech.cowboyandalien;
+
+public class FelixKannJetzAuchKlassenErstellen {
+
+	public FelixKannJetzAuchKlassenErstellen() {
+
+		for (int i = 0; i < 100; i++) {
+			System.out.println("Ihr Lappen");
+		}
+
+	}
+
+}
