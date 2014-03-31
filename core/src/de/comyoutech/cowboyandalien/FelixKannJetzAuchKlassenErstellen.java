5af50b43650c42ff57685e67a59d0a540bb3c7ec
flexus385 <lappen@lappen.de>
2014-03-31 21:42:22 +0200

    Error

--- core/src/de/comyoutech/cowboyandalien/FelixKannJetzAuchKlassenErstellen.java
+++ core/src/de/comyoutech/cowboyandalien/FelixKannJetzAuchKlassenErstellen.java
@@ -6,6 +6,22 @@ public class FelixKannJetzAuchKlassenErstellen {
 
 		for (int i = 0; i < 100; i++) {
 			System.out.println("Ihr Lappen");
+		
+//			
+//			Exception in thread "main" java.lang.NoClassDefFoundError: com/badlogic/gdx/jnigen/NativeCodeGenerator
+//			at com.badlogic.gdx.utils.GdxBuild.main(GdxBuild.java:34)
+//		Caused by: java.lang.ClassNotFoundException: com.badlogic.gdx.jnigen.NativeCodeGenerator
+//			at java.net.URLClassLoader$1.run(Unknown Source)
+//			at java.net.URLClassLoader$1.run(Unknown Source)
+//			at java.security.AccessController.doPrivileged(Native Method)
+//			at java.net.URLClassLoader.findClass(Unknown Source)
+//			at java.lang.ClassLoader.loadClass(Unknown Source)
+//			at sun.misc.Launcher$AppClassLoader.loadClass(Unknown Source)
+//			at java.lang.ClassLoader.loadClass(Unknown Source)
+//			... 1 more
+		
+		
+		
 		}
 
 	}
