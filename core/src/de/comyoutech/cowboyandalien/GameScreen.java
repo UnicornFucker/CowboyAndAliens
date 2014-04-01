f344c9170440f2b0efa15f1ecd2e0dda55b0642b
flexus385 <lappen@lappen.de>
2014-04-01 08:52:35 +0200

    GameScreen

---
+++ core/src/de/comyoutech/cowboyandalien/GameScreen.java
@@ -0,0 +1,74 @@
+package de.comyoutech.cowboyandalien;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class GameScreen implements Screen {
+	
+	MyGdxGame game;
+	OrthographicCamera camera;
+	SpriteBatch batch;
+	
+	public GameScreen(MyGdxGame game){
+		this.game = game;
+		camera = new OrthographicCamera();
+		camera.setToOrtho(true, 1920, 1080);
+		batch = new SpriteBatch();
+
+	}
+
+	@Override
+	public void render(float delta) {
+		Gdx.gl.glClearColor(0.95F, 0.95F, 0.95F, 0.95F);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		camera.update();
+		
+		batch.setProjectionMatrix(camera.combined);
+
+		batch.begin();
+		
+		batch.end();
+		
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void show() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void hide() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
