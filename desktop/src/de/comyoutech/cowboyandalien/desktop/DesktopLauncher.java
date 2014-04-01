b70c2bf69622228fd8b75de7a4b027b01b646de8
flexus385 <lappen@lappen.de>
2014-04-01 09:29:46 +0200

    config angepasst

--- desktop/src/de/comyoutech/cowboyandalien/desktop/DesktopLauncher.java
+++ desktop/src/de/comyoutech/cowboyandalien/desktop/DesktopLauncher.java
@@ -5,8 +5,14 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import de.comyoutech.cowboyandalien.MyGdxGame;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
+	public static void main(String[] arg) {
+		
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+
+		config.title = "CowboysAndAliens";
+		config.width = 480;
+		config.height = 320;
+
 		new LwjglApplication(new MyGdxGame(), config);
 	}
 }
