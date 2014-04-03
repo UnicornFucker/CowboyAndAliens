b59cbd13272afedf5c1b43b43019b5e2c4cf6756
BembelDesTodes <ganjaterror@hotmail.de>
2014-04-03 08:16:12 +0200

    Georg Zeug 2

---
+++ core/src/de/comyoutech/cowboyandalien/MyGdxGame.java
@@ -0,0 +1,43 @@
+package de.comyoutech.cowboyandalien;
+
+import com.badlogic.gdx.Game;
+
+/** Import **/
+
+public class MyGdxGame extends Game {
+
+    public void create() {
+        setScreen(new GameScreen());
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void render() {
+        // TODO Auto-generated method stub
+        super.render();
+    }
+
+    @Override
+    public void pause() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void resume() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void dispose() {
+        // TODO Auto-generated method stub
+
+    }
+
+}
