1a139c0c9629b4a93f2655a167b7910e295aa028
flexus385 <lappen@lappen.de>
2014-04-07 08:49:15 +0200

    Neue Images

--- core/src/de/comyoutech/cowboyandalien/Assets.java
+++ core/src/de/comyoutech/cowboyandalien/Assets.java
@@ -1,11 +1,25 @@
 package de.comyoutech.cowboyandalien;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.Texture.TextureFilter;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
 public class Assets {
 
-    public Assets() {
-    }
+	public static Sound jump_sound;
+	public static Sound jump_sound;
+	public static Sound jump_sound;
+	public static Sound explosion;
 
-    public static void load() {
-    }
+	public Assets() {
+	}
 
+	public static void load() {
+		coolsound = Gdx.audio.newSound(Gdx.files.internal("jump.wav"));
+
+	}
 }
