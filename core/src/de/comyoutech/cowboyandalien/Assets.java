c8ea167805d89fc647889ae545fbfb3a953350d5
BembelDesTodes <ganjaterror@hotmail.de>
2014-06-11 08:30:18 +0200

    sdad

--- core/src/de/comyoutech/cowboyandalien/Assets.java
+++ core/src/de/comyoutech/cowboyandalien/Assets.java
@@ -19,15 +19,21 @@ public class Assets {
     private static final float RUNNING_FRAME_DURATION = 0.06f;
 
     public static Texture explosion_texture;
-    public static Texture texturePlayer;
-    public static Sprite explosion_sprite;
     public static TextureRegion playerLeft;
+
+    /* Background Textures */
+
+    public static Texture textureBackground_level1;
+    public static Sprite background_sprite_level1;
+    public static Texture textureBackground_level2;
+    public static Sprite background_sprite_level2;
+    // public static Texture textureBackground_level3;
+    // public static Sprite background_sprite_level3;
 
     public static TextureRegion idleLeft;
     public static TextureRegion idleRight;
     public static TextureRegion blockTextureGround;
     public static TextureRegion blockTexturePlatform;
-
     public static Animation leftAnimation;
     public static Animation rightAnimation;
     public static TextureRegion jumpLeft;
@@ -57,6 +63,7 @@ public class Assets {
         // loadSounds();
 
         loadPlayerTextures();
+        loadBackgrounds();
 
     }
 
@@ -64,13 +71,12 @@ public class Assets {
         TextureAtlas atlas = new TextureAtlas(
                 Gdx.files.internal("images/textures/textures.pack"));
         idleLeft = atlas.findRegion("bob-01");
-        idleRight = new TextureRegion(idleLeft);
-        idleRight.flip(true, false);
         blockTextureGround = new TextureRegion(new Texture(
                 Gdx.files.internal("images/block_neu3.png")));
         blockTexturePlatform = new TextureRegion(new Texture(
                 Gdx.files.internal("images/block_neu2.png")));
-
+        idleRight = new TextureRegion(idleLeft);
+        idleRight.flip(true, false);
         TextureRegion[] walkLeftFrames = new TextureRegion[5];
         for (int i = 0; i < 5; i++) {
             walkLeftFrames[i] = atlas.findRegion("bob-0" + (i + 2));
@@ -101,6 +107,22 @@ public class Assets {
         background_sound = getSound("sounds/background_music.mp3");
     }
 
+    private static void loadBackgrounds() {
+        textureBackground_level1 = new Texture(
+                Gdx.files.internal("backgrounds/background.png"));
+
+        background_sprite_level1 = new Sprite(textureBackground_level1);
+
+        background_sprite_level1.flip(true, false);
+        textureBackground_level2 = new Texture(
+                Gdx.files.internal("backgrounds/background2.jpg"));
+
+        background_sprite_level2 = new Sprite(textureBackground_level2);
+
+        background_sprite_level2.flip(true, false);
+
+    }
+
     public static TextureRegion getPlayerTextureRight() {
 
         explosion_texture = null;
@@ -111,4 +133,5 @@ public class Assets {
     private static Sound getSound(String path) {
         return Gdx.audio.newSound(Gdx.files.internal(path));
     }
+
 }
