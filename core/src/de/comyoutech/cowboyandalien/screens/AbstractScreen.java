703220943c3d93d53a6f9b99bf09d58fc4648b17
LeoIll <BrookZ@10.4.19.113>
2014-06-26 11:19:08 +0200

    g

---
+++ core/src/de/comyoutech/cowboyandalien/screens/AbstractScreen.java
@@ -0,0 +1,31 @@
+package de.comyoutech.cowboyandalien.screens;
+
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+import de.comyoutech.cowboyandalien.controller.MyGdxGame;
+
+/**
+ * The abstract screen class.
+ * 
+ * @author BrookZ
+ * 
+ */
+public abstract class AbstractScreen implements Screen, InputProcessor {
+
+    /**
+     * The batch for drawing.
+     */
+    protected SpriteBatch batch;
+    /**
+     * Game-object.
+     */
+    protected MyGdxGame game;
+
+    public AbstractScreen(MyGdxGame game) {
+        this.game = game;
+        batch = new SpriteBatch();
+    }
+
+}
